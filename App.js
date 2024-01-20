b0940d47a8ff00b939ac9857deb991e63efe08a5
Bruno Bonomo <bruno.bonomo11@gmail.com>
2024-01-20 02:19:13 -0300

    Agregamos textos y cambiamos estilos a nuestra App

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Bienvenidos a nuestra primera App !</Text>
-      <Text>Creamos nuestro primer proyecto con React Native</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+  
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Bienvenidos a nuestra primera App !</Text>
+        <Text style={styles.titulo}>Creamos nuestro primer proyecto con React Native</Text>
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E9783B',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  titulo: {
+    textTransform: 'uppercase',
+    fontSize: 15,
+    color: '#FFFFFF'
+  }
 });
+
+export default App
