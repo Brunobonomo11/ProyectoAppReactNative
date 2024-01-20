f207572493e5c8a245f2b324e0b3ed525d1154cf
Bruno Bonomo <bruno.bonomo11@gmail.com>
2024-01-20 02:04:17 -0300

    Creamos nuestra primera App con React Native

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Bienvenidos a nuestra primera App !</Text>
+      <Text>Creamos nuestro primer proyecto con React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
