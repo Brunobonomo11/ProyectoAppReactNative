4ad996b6f924144b663bc6e8697c3d583321a27c
brunobonomo11 <bruno.bonomo11@gmail.com>
2024-01-25 00:13:55 -0300

    Agregamos cajas con el boton iniciar viaje, un app para viajar en taxi, cada caja muestra el pasajero

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button} from 'react-native';
 
 const App = () => {
   
     return (
       <View style={styles.container}>
-        <Text style={styles.titulo}>Bienvenidos a nuestra primera App !</Text>
-        <Text style={styles.titulo}>Creamos nuestro primer proyecto con React Native</Text>
+        <TextInput placeholder='Ingrese su nombre de usuario' style={styles.inputUser} />
+        <TextInput placeholder='Ingrese su contraseña' style={styles.inputUser} />
+        <Button style={styles.botonInicio} title='INICIAR SESION' />
+      <View style={styles.containerTitulo}>
+                    <Text style={styles.titulo}>INICIO</Text>
+                    <Text style={styles.titulo}>VIAJES</Text>
+                    <Text style={styles.titulo}>TARIFAS</Text>
+                    <Text style={styles.titulo}>SOBRE NOSOTROS</Text>
+                    <Text style={styles.titulo}>CONTACTO</Text>
+      </View>
+      <View style={styles.boxContainer}>
+          <View style={styles.box}>
+            <Text style={styles.textPasajero}>
+                Pasajero 1
+            </Text>
+            <Button color="#E8F227" style={styles.botonViaje} title='Iniciar Viaje' />
+          </View>
+          <View style={styles.box}>
+            <Text style={styles.textPasajero}>
+                Pasajero 2
+            </Text>
+            <Button color="#E8F227" style={styles.botonViaje} title='Iniciar Viaje' />
+          </View>
+          <View style={styles.box}>
+            <Text style={styles.textPasajero}>
+                Pasajero 3
+            </Text>
+            <Button color="#E8F227" style={styles.botonViaje} title='Iniciar Viaje' />
+          </View>
+          <View style={styles.box}>
+            <Text style={styles.textPasajero}>
+                Pasajero 4
+            </Text>
+            <Button color="#E8F227" style={styles.botonViaje} title='Iniciar Viaje' />
+          </View>
+      </View>
       </View>
     );
 }
@@ -13,14 +47,45 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E9783B',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#F7FF58',
+    paddingTop: 50,
   },
   titulo: {
     textTransform: 'uppercase',
-    fontSize: 15,
-    color: '#FFFFFF'
+    fontSize: 20,
+    color: '#000'
+  },
+  inputUser: {
+    borderWidth:2, 
+    margin:10, 
+    paddingVertical:5, 
+    paddingHorizontal:10
+  },
+  containerTitulo: {
+    display: 'flex',
+    alignItems: 'start'
+  },
+  boxContainer: {
+    backgroundColor: '#525350',
+    alignItems: 'center',
+    gap: 25,
+    padding: 10,
+    marginTop: 15
+  },
+  box: {
+    backgroundColor: '#000',
+    flexDirection: 'row',
+    padding: 35
+  },
+  textPasajero: {
+    width: '70%',
+    color: '#fff',
+    alignItems: 'center',
+    marginTop: 8
+  },
+  botonViaje: {
+    color: '#fff',
+    backgroundColor: '#000',
   }
 });
 
