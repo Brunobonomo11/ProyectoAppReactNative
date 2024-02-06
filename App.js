e93bdea8e22cb7f42c6719c1e85f27acafa016c7
brunobonomo11 <bruno.bonomo11@gmail.com>
2024-02-06 00:37:42 -0300

    Agregamos categorias a la app, y cada vez que buscamos un producto se almacena en la consola

--- App.js
+++ App.js
@@ -1,96 +1,102 @@
+import { useState } from 'react';
 import { StyleSheet, Text, TextInput, View, Button} from 'react-native';
 
 const App = () => {
+      const [newProduct, setNewProduct] = useState({})
+      const [product, setProduct] = useState([])
 
-  const addTask = () => {
-    console.log("Se acaba de iniciar el Viaje")
-  }
+      const addProduct = () =>{
+        setProduct([...product, setProduct])
+        setNewProduct({
+          title:"",
+          categoria:""
+        })
+      }
+
+      const onHandlerTitle = (t) =>{
+        setNewProduct({...newProduct, title:t})
+      }
+
+      const onHandlerCategoria = (t) => {
+        setNewProduct({...newProduct, categoria:t})
+      }
   
-    return (
-      <View style={styles.container}>
-        <TextInput placeholder='Ingrese su nombre de usuario' style={styles.inputUser} />
-        <TextInput placeholder='Ingrese su contraseña' style={styles.inputUser} />
-        <Button style={styles.botonInicio} title='INICIAR SESION' />
-      <View style={styles.containerTitulo}>
-                    <Text style={styles.titulo}>INICIO</Text>
-                    <Text style={styles.titulo}>VIAJES</Text>
-                    <Text style={styles.titulo}>TARIFAS</Text>
-                    <Text style={styles.titulo}>SOBRE NOSOTROS</Text>
-                    <Text style={styles.titulo}>CONTACTO</Text>
-      </View>
-      <View style={styles.boxContainer}>
-          <View style={styles.box}>
-            <Text style={styles.textPasajero}>
-                Pasajero 1
-            </Text>
-            <Button color="#E8F227" style={styles.botonViaje} title='Iniciar Viaje' onPress={addTask} />
-          </View>
-          <View style={styles.box}>
-            <Text style={styles.textPasajero}>
-                Pasajero 2
-            </Text>
-            <Button color="#E8F227" style={styles.botonViaje} title='Iniciar Viaje' onPress={addTask} />
-          </View>
-          <View style={styles.box}>
-            <Text style={styles.textPasajero}>
-                Pasajero 3
-            </Text>
-            <Button color="#E8F227" style={styles.botonViaje} title='Iniciar Viaje' onPress={addTask} />
-          </View>
-          <View style={styles.box}>
-            <Text style={styles.textPasajero}>
-                Pasajero 4
-            </Text>
-            <Button color="#E8F227" style={styles.botonViaje} title='Iniciar Viaje' onPress={addTask} />
-          </View>
-      </View>
-      </View>
-    );
+      return (
+        <View style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput value={newProduct.title} onChangeText={onHandlerTitle} placeholder='Ingresar Producto' style={styles.input}/>
+                <TextInput value={newProduct.categoria} onChangeText={onHandlerCategoria} placeholder='Ingresar Categoria' style={styles.input}/>
+                <Button color="#FFC09F" title='Buscar Producto' onPress={addProduct}/>
+            </View>
+              <View style={styles.packContainer}>
+                <View style={styles.pack}>
+                    <Text style={styles.textPack}>Smartwatch</Text>
+                    <Button color="#FFC09F" title='Ver Más' />
+                </View>
+                <View style={styles.pack}>
+                    <Text style={styles.textPack}>Auriculares</Text>
+                    <Button color="#FFC09F" title='Ver Más' />
+                </View>
+                <View style={styles.pack}>
+                    <Text style={styles.textPack}>Celulares</Text>
+                    <Button color="#FFC09F" title='Ver Más' />
+                </View>
+                <View style={styles.pack}>
+                    <Text style={styles.textPack}>Parlantes</Text>
+                    <Button color="#FFC09F" title='Ver Más' />
+                </View>
+                <View style={styles.pack}>
+                    <Text style={styles.textPack}>Teclados</Text>
+                    <Button color="#FFC09F" title='Ver Más' />
+                </View>
+                <View style={styles.pack}>
+                    <Text style={styles.textPack}>Pantallas / Monitores</Text>
+                    <Button color="#FFC09F" title='Ver Más' />
+                </View>
+              </View>
+        </View>
+      )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F7FF58',
-    paddingTop: 50,
-  },
-  titulo: {
-    textTransform: 'uppercase',
-    fontSize: 20,
-    color: '#000'
-  },
-  inputUser: {
-    borderWidth:2, 
-    margin:10, 
-    paddingVertical:5, 
-    paddingHorizontal:10
-  },
-  containerTitulo: {
-    display: 'flex',
-    alignItems: 'start'
-  },
-  boxContainer: {
-    backgroundColor: '#525350',
-    alignItems: 'center',
-    gap: 25,
-    padding: 10,
-    marginTop: 15
-  },
-  box: {
-    backgroundColor: '#000',
-    flexDirection: 'row',
-    padding: 35
-  },
-  textPasajero: {
-    width: '70%',
-    color: '#fff',
-    alignItems: 'center',
-    marginTop: 8
-  },
-  botonViaje: {
-    color: '#fff',
-    backgroundColor: '#000',
-  }
+    container: {
+        backgroundColor: "#8AC6D0",
+        flex: 1,
+        paddingTop: 30
+      },
+    inputContainer: {
+        alignItems: "center",
+        justifyContent: "space-around",
+        paddingTop: 15
+    },
+    input: {
+        width: 250,
+        borderBottomWidth: 2,
+        borderColor: "white",
+        margin: 10,
+        paddingVertical: 5,
+        paddingHorizontal: 10
+      },
+    text: {
+        fontSize: 16
+    },
+    packContainer: {
+        alignItems: "center",
+        gap: 25,
+        padding: 10
+    },
+    pack: {
+        backgroundColor: "#554971",
+        flexDirection: "row",
+        padding: 20,
+        alignItems: "center",
+        borderRadius: 5
+    },
+    textPack: {
+        width: "70%",
+        color: "white",
+        fontSize: 13
+    }
 });
 
 export default App
